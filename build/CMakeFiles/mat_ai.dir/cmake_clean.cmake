4568da60f2bc76937a0118cafae8e3afcf3de521
nguyenvantam00 <ntoibiet@gmail.com>
2025-03-25 20:05:18 +0700

    Cập nhật tính năng mới

---
+++ build/CMakeFiles/mat_ai.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mat_ai.dir/src/main.cpp.o"
+  "CMakeFiles/mat_ai.dir/src/main.cpp.o.d"
+  "mat_ai"
+  "mat_ai.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mat_ai.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+
+
+#include <iostream>
+
+
+int main() {
+	std::cout << "hello work!" << std::endl;
+	return 0;
+}
